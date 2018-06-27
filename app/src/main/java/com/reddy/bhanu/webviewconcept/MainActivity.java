8bf391f0293db008f3af45edb6b026f194f29ec8
suryapraveen007 <37669937+suryapraveen007@users.noreply.github.com>
2018-06-27 23:17:50 +0530

    first commit

---
+++ app/src/main/java/com/reddy/bhanu/webviewconcept/MainActivity.java
@@ -0,0 +1,35 @@
+package com.reddy.bhanu.webviewconcept;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://www.facebook.com/");
+        myWebView.setWebViewClient(new WebViewController());
+
+    }
+
+    public class WebViewController extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+}
